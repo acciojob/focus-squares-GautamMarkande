a953a2c2b97a9d922504c827b2de05663dab19e3
GautamMarkande <118103083+GautamMarkande@users.noreply.github.com>
2024-01-18 17:28:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,21 +5,21 @@ const s3=document.getElementById('square3');
 
 s1.addEventListener('mouseover',()=>{
 	// s2.innerText = "ok"
-	s1.style.backgroundColor="#E6E6FA"
-	s2.style.backgroundColor="#6F4E37"
-	s3.style.backgroundColor="#6F4E37"
+	s1.style.backgroundColor="rgb(230, 230, 250)"
+	s2.style.backgroundColor="rgb(111, 78, 55)"
+	s3.style.backgroundColor="rgb(111, 78, 55)"
 })
 s2.addEventListener('mouseover',()=>{
 	// s2.innerText = "ok"
-	s2.style.backgroundColor="#E6E6FA"
-	s1.style.backgroundColor="#6F4E37"
-	s3.style.backgroundColor="#6F4E37"
+	s2.style.backgroundColor="rgb(230, 230, 250)"
+	s1.style.backgroundColor="rgb(111, 78, 55)"
+	s3.style.backgroundColor="rgb(111, 78, 55)"
 })
 s3.addEventListener('mouseover',()=>{
 	// s2.innerText = "ok"
-	s3.style.backgroundColor="#E6E6FA"
-	s2.style.backgroundColor="#6F4E37"
-	s1.style.backgroundColor="#6F4E37"
+	s3.style.backgroundColor="rgb(230, 230, 250)"
+	s2.style.backgroundColor="rgb(111, 78, 55)"
+	s1.style.backgroundColor="rgb(111, 78, 55)"
 })
 
 
